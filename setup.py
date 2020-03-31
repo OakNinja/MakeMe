f778049e25eb524b8f62231c1e16121fd505a8a8
Esse Woods <oakninja@gmail.com>
2020-03-31 23:08:59 +0200

    0.1.6 Version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(filename):
 
 setup(
     name="MakeMe",
-    version="0.1.5",
+    version="0.1.6",
     author="Esse Woods",
     author_email="esse.woods@gmail.com",
     description="Easing the usage of Makefiles",
